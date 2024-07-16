beacaa60614910918ccd482be2631bbac37ed963
zaindgr8 <66069515+zaindgr8@users.noreply.github.com>
2024-07-16 17:48:48 +0400

    548

--- api/team.js
+++ api/team.js
@@ -10,39 +10,38 @@ import tsimg4 from '/public/images/team/ts4.jpg'
 
 
 const Teams = [
-    {
-       id:'1',
-       tImg:timg1,
-       tsimg:tsimg1,
-       name:'Hamna Basharat',
-       slug:'Hamna-Basharat',
-       title:'President',     
-    },
-    {
-       id:'2',
-       tImg:timg2,
-       tsimg:tsimg2,
-       name:'Kamran Ali',
-       slug:'Kamran',
-       title:'General Manager',     
-    },
-    {
-       id:'3',
-       tImg:timg3,
-       tsimg:tsimg3,
-       name:'Amna Ashfaque',
-       slug:'Amna',
-       title:'Marketing Head',     
-    },
-    {
-       id:'4',
-       tImg:timg4,
-       tsimg:tsimg4,
-       name:'Saifullah',
-       slug:'Saifullah',
-       title:'Volunteer',     
-    }
-    
-]
+  {
+    id: "1",
+    tImg: timg1,
+    tsimg: tsimg1,
+    name: "Hamna Basharat",
+    slug: "Hamna-Basharat",
+    title: "President",
+  },
+  {
+    id: "2",
+    tImg: timg2,
+    tsimg: tsimg2,
+    name: "Kamran Ali",
+    slug: "Kamran",
+    title: "General Manager",
+  },
+  {
+    id: "3",
+    tImg: timg3,
+    tsimg: tsimg3,
+    name: "Amna Shafiq",
+    slug: "Amna",
+    title: "Marketing Head",
+  },
+  {
+    id: "4",
+    tImg: timg4,
+    tsimg: tsimg4,
+    name: "Saifullah",
+    slug: "Saifullah",
+    title: "Volunteer",
+  },
+];
 
 export default Teams;
